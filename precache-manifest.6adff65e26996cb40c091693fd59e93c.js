ee12acc81692b1915513c61603476404a1ce7b53
EsonJunior <jaysonjrhernandez05@gmail.com>
2022-08-19 17:40:16 +0800

    Updates

---
+++ precache-manifest.6adff65e26996cb40c091693fd59e93c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "16834bdff6aefac528bb",
+    "url": "/static/css/main.6986bdc9.chunk.css"
+  },
+  {
+    "revision": "16834bdff6aefac528bb",
+    "url": "/static/js/main.16834bdf.chunk.js"
+  },
+  {
+    "revision": "41e54f60bbc7d736b66d",
+    "url": "/static/js/1.41e54f60.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5b7407974f158be193fe76a06fc31f5c",
+    "url": "/static/media/jayson-hernandez-resume-2022.5b740797.pdf"
+  },
+  {
+    "revision": "483b6583bf8988102f183376f7860b84",
+    "url": "/static/media/me.483b6583.png"
+  },
+  {
+    "revision": "7293c9246f9250dda461976f9de27d06",
+    "url": "/static/media/me-about.7293c924.jpg"
+  },
+  {
+    "revision": "f5f338242cc3517ac5cba18f8eaefeb6",
+    "url": "/static/media/portfolio1.f5f33824.jpg"
+  },
+  {
+    "revision": "8f7132c79f40ae61760ee9779549b707",
+    "url": "/static/media/portfolio2.8f7132c7.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "3a8538c4c0884a4782a9075053ae4839",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website-profile/precache-manifest.bfb7dab4e1d68a7957ac9b79678f1e48.js"
+  "/precache-manifest.6adff65e26996cb40c091693fd59e93c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/website-profile/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
